a909cc77885d839892dfe62188b00e02bd94f617
Gulu <gulusv19999@gmail.com>
2022-09-24 18:46:34 +0400

    Router added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
-// import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React app
-        </a>
-      </header>
-    </div>
-
->>>>>>> f846549bdf58accd432feaacf5e0cc688f6a0e96
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import '../Header/header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+     <div className="header-top">
+      <div className="phone">
+        <p>+1235 2355 98</p>
+      </div>
+     </div>
+    </div>
   )
 }
 
