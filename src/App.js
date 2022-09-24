bbef057834dfc2a488828729d2a100c4e69e0c92
Jafar1i <103290548+Jafarl1@users.noreply.github.com>
2022-09-24 19:48:22 +0400

    App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
 import Home from "./components/Pages/Home/Home";
 
 function App() {
   return (
     <>
-      <Home/ >
+      <Home />
     </>
-  );
-=======
-import React from 'react'
-
-function App() {
-  return (
-    <div>App</div>
-  )
->>>>>>> baf770b4b25100b016c3d7da4302f6437d7d07e2
-}
+  )}
 
 export default App
